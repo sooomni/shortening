08dc86cbde11588225a9a00e21fde48385162b05
Soomin Kim <soomin0403@naver.com>
2021-05-19 23:55:41 +0900

    first commit

---
+++ src/test/java/com/musinsa/shortening/ShorteningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.shortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShorteningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
